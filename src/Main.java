f13259ddde1c20c3cb98d6e12dc16723cd118499
hyg0527 <yoongoo0807@gmail.com>
2023-12-31 18:07:16 +0900

    git received2

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,7 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
-        System.out.println("clone succeeded!");
-        System.out.println(Add.add(2, 3));
-        System.out.println(Add.add(200,200));
+        System.out.println("clone succeeded!2");
     }
 }
 
