b5874f8350c95840371fc54e22872a56ee4a07cd
hyg0527 <yoongoo0807@gmail.com>
2023-12-31 18:04:57 +0900

    clone received

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,12 @@ public class Main {
             System.out.println(i);
         }
         System.out.println("clone succeeded!");
+        System.out.println(Add.add(2, 3));
+    }
+}
+
+class Add {
+    public static int add(int x, int y) {
+        return x + y;
     }
 }
