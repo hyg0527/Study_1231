8c72b6a2a60a7ca8db28422f9b96005c784c3f62
hyg0527 <yoongoo0807@gmail.com>
2023-12-31 19:05:50 +0900

    develop2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
-        System.out.println("clone succeeded!2");
+        System.out.println("clone succeeded! develop branch");
 
         Develop.run();
     }
